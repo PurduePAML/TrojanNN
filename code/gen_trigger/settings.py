576ea5651e181ed7d0282c0844cb9fe365dd8dfd
Yingqi Liu <naiyeleo@gmail.com>
2019-01-24 15:56:58 -0500

    add commet

--- code/gen_trigger/settings.py
+++ code/gen_trigger/settings.py
@@ -1,4 +1,5 @@
 caffe_root = "/path/to/caffe/python" 
 model_path = "/path/to/VGG_FACE.caffemodel"
+# add  'force_backward: true' in the prototxt file otherwise the caffe does not do backward computation and gradient is 0
 model_definition   = '/path/to/VGG_FACE_deploy.prototxt'
 gpu = False
